5e3c99c0c99a105752e6fb80714f2653eea00be4
张乾泽 <qianze.zhang@hotmail.com>
2018-12-13 20:39:41 +0800

    switch to broadcasting mode

--- App.js
+++ App.js
@@ -23,13 +23,17 @@ import AgoraRendererView from './components/AgoraRendererView'
 
 const agoraKitEmitter = new NativeEventEmitter(AgoraRtcEngine);
 var isSpeakerPhone = false;
+//provide this url if you want rtmp relevant features
+var cdn_url = "YOUR_CDN_URL"
 
 export default class App extends Component {
   
 // Agora Action 
   _joinChannel() {
     AgoraRtcEngine.setLocalVideoView(this._localView, AgoraRtcEngine.AgoraVideoRenderModeFit);
-    AgoraRtcEngine.setVideoProfile(AgoraRtcEngine.AgoraVideoProfileDEFAULT, false);
+    AgoraRtcEngine.setChannelProfile(1);
+    AgoraRtcEngine.setClientRole(1);
+    AgoraRtcEngine.setVideoProfile(AgoraRtcEngine.AgoraVideoProfileDEFAULT, true);
     AgoraRtcEngine.startPreview();
     AgoraRtcEngine.joinChannel(null, "rnchannel01", "React Native for Agora RTC SDK", 0);
   }
@@ -48,9 +52,17 @@ export default class App extends Component {
     isSpeakerPhone = !isSpeakerPhone;
   }
   
+  _startPublish() {
+    AgoraRtcEngine.addPublishStreamUrl(cdn_url, false)
+  }
+
+  _stopPublish() {
+    AgoraRtcEngine.removePublishStreamUrl(cdn_url)
+  }
+
   render() {
     
-    AgoraRtcEngine.createEngine('YOUR APP ID');
+    AgoraRtcEngine.createEngine("YOUR_APP_ID");
 
     AgoraRtcEngine.enableVideo();
     AgoraRtcEngine.enableAudio();
@@ -98,6 +110,19 @@ export default class App extends Component {
           />
       </View>
 
+      <View style={{flexDirection: 'row'}}>
+          <Button
+            onPress={this._startPublish.bind(this)}
+            title="Start publish"
+            color="#841584"
+          />
+          <Button
+            onPress={this._stopPublish.bind(this)}
+            title="Stop publish"
+            color="#841584"
+          />
+      </View>
+
     </View>
     );
   }
@@ -110,8 +135,23 @@ export default class App extends Component {
     }
   );
 
+  addPublishStreamUrlNoti = agoraKitEmitter.addListener(
+    'StreamPublished',
+    (notify) => {
+      console.log(`stream published ${notify.errorCode}`)
+    }
+  );
+
+  removePublishStreamUrlNoti = agoraKitEmitter.addListener(
+    'StreamUnpublished',
+    (notify) => {
+      console.log(`stream unpublished`)
+    }
+  );
+
   componentWillUnmount() {
     remoteDidJoineChannelNoti.remove()
+    addPublishStreamUrlNoti.remove()
   }
   
 }
